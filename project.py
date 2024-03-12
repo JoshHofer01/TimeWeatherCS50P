b1de6199afa532aeec0bd315f56f55d29e3ec2c8
Josh H <josh@securesap.com>
2024-03-12 21:26:11 +0000

    removed class 'User Commands' as unncessary for this type of programm. Added docstrings/comments and removed code duplication

--- project.py
+++ project.py
@@ -5,132 +5,27 @@ import os
 import sys
 from dotenv import load_dotenv
 
-class User_commands():
-    def __init__(self, timestyle=None, temptype=None, details=False):
-        self.timestyle = timestyle
-        self.temptype = temptype
-        self.details = details
-        self.locations = pytz.common_timezones
-        self.city = None
-        self.city2 = None
-        self.continent = None
-        self.get_user_commands()
-
-
-    def get_user_commands(self):
-        while True:
-            timestyle = input("'24' or '12' hour time format: ").strip()
-            if timestyle == '24' or timestyle == '12':
-                self.timestyle = timestyle
-                break
-            else:
-                print("Invalid input, please enter '24' or '12'.")
-
-        while True:
-            details = input("Would you like detailed weather info: (Y/N) ").strip().upper()
-            if details == 'N':
-                return
-            #Getting details
-            elif details == 'Y':
-                #Get temptype
-                while True:
-                    temptype = input("Celsius(C) or Fahrenheit(F) temperature format: ").strip().upper()
-                    if temptype == 'C' or temptype == 'F':
-                        self.temptype = temptype
-                        self.details = True
-                        break
-                    else:
-                        print("Invalid input, please enter 'C' or 'F'.")
-                break
-
-
-    def get_location(self):
-        continent_options = ['Africa','America','Asia','Antarctica','Australia','Europe']
-        cities = []
-        cities2 = []
-
-        print("Type the number corresponding with your choice: ")
-        for index, continent in enumerate(continent_options, start=1):
-            print(f"{index}. {continent}")
-
-        while True:
-            continent_choice = input("Continent: ")
-            try:
-                continent_choice = continent_options[int(continent_choice) - 1]
-            except:
-                print(f"Invalid input, please enter a number between 1-{len(continent_options)}.")
-            if continent_choice in continent_options:
-                # Adds all supported cities in chosen continent to list
-                for location in self.locations:
-                    if '/' in location and location.startswith(continent_choice):
-                        continent, city = location.split('/', 1)
-                        if '/' not in city:
-                            if city not in cities:
-                                cities.append(city)
-                        elif '/' in city:
-                            if city.split('/')[0]not in cities:
-                                cities.append(city.split('/')[0])
-                break
-
-        # Prints all supported cities
-        print(f"Cities in {continent_choice}, select a number corresponding with your choice:")
-        for index, city in enumerate(cities, start=1):
-            print(f"{index}. {city}")
-
-        while True:
-            city = input("City: ")
-            try:
-                city = cities[int(city) - 1]
-                break
-            except:
-                print(f"Invalid input, please enter a number between 1-{len(cities)}.")
-
-        self.city = city
-        self.continent = continent_choice
-        if city in ['Argentina', 'North_Dakota', 'Kentucky', 'Indiana']:
-            for location in self.locations:
-                if location.startswith(f"{continent_choice}/{city}") and location not in cities2:
-                    cities2.append(location.split('/')[2])
-            for index, city in (enumerate(cities2, start=1)):
-                print(f"{index}. {city}")
-
-            # returns inputs to class methods
-            while True:
-                city2 = input("City: ")
-                try:
-                    city2 = cities2[int(city2) - 1]
-                    break
-                except:
-                    print(f"Invalid input, please enter a number between 1-{len(cities2)}.")
-            self.city2 = city2
-            self.continent = continent_choice
-
 
 def main():
     print("""Time & Weather information.
 CS50P Final Project by Josh Hofer - England""")
     #user commands variable must be in main otherwise interfers with pytest.
     #assigns variables to methods in class
-    user_commands = User_commands()
-    temptype = user_commands.temptype
-    timestyle = user_commands.timestyle
-    location = user_commands.get_location()
-    continent = user_commands.continent
-    city = user_commands.city
-    city2 = user_commands.city2
-    
-    if city2 == None:
-        location = f"{continent}/{city}"
+    timestyle, temptype, details = get_user_commands()
+    city, city2, continent = get_location()
+    if not city2:
+        location = "/".join([continent, city])
     else:
-        location = f"{continent}/{city}/{city2}"
+        location = "/".join([continent, city, city2])
 
     try:
         openweather_raw = get_openweather_info(city)
     except:
         sys.exit("ERROR: Missing or Invalid API Key, please revisit environment variable to check API Key.")
+
     if city2 != None:
             city = city2
-    if user_commands.details == True:
+    if details:
         print(f"You chose {continent}, {city.replace('_', ' ')}!")
         print(f"The time in {city.replace('_', ' ')} is {get_time(location, timestyle)}")
         try:
@@ -153,6 +48,7 @@ def get_weather(openweather_raw):
     wind_speed = openweather_raw['wind']['speed']
     return humidity, wind_speed
 
+
 def get_temp(openweather_raw, temptype):
     temp_kelvin = openweather_raw['main']['temp']
     if temptype == 'C':
@@ -161,6 +57,7 @@ def get_temp(openweather_raw, temptype):
     elif temptype == 'F':
         temp_fahrenheit = 1.8 * (temp_kelvin - 273) + 32
         return f"{temp_fahrenheit:.2f}°F"
+
 
 def feelslike_statement(openweather_raw, temptype):
     temp_kelvin = openweather_raw['main']['feels_like']
@@ -203,6 +100,111 @@ def get_openweather_info(city):
     url = f"{base_url}appid={api_key}&q={city}"
     response = requests.get(url).json()
     return response
+
+
+def get_user_commands():
+    """Gets general information from user and returns 3 variables for use in programm """
+    while True:
+        timestyle = input("'24' or '12' hour time format: ").strip()
+        if timestyle == '24' or timestyle == '12':
+            break
+        else:
+            print("Invalid input, please enter '24' or '12'.")
+
+    while True:
+        details = input("Would you like detailed weather info: (Y/N) ").strip().upper()
+        if details == 'N':
+            details = False
+            temptype = None
+            return timestyle, temptype, details
+
+        #Getting details
+        elif details == 'Y':
+            details = True
+            #Get temptype
+            while True:
+                temptype = input("Celsius(C) or Fahrenheit(F) temperature format: ").strip().upper()
+                if temptype in ["C", "F"]:
+                    return timestyle, temptype, details
+                else:
+                    print("Invalid input, please enter 'C' or 'F'.")
+
+
+def get_location():
+    """First part of function populates the cities list with location info pytz
+    timezone module."""
+    LOCATIONS = pytz.common_timezones # Gathers list in format xx/xx/xx from pytz module
+    continent_options = ['Africa','America','Asia','Antarctica','Australia','Europe']
+    cities = []
+    cities2 = []
+
+    print("Type the number corresponding with your choice: ")
+    for index, continent in enumerate(continent_options, start=1):
+        print(f"{index}. {continent}")
+
+    while True:
+        try:
+            continent_number = int(input("Continent: "))
+            if continent_number >= 1 and continent_number <= len(continent_options):
+                break
+            else:
+                print(f"Invalid input, please enter a number between 1-{len(continent_options)}.")
+        except ValueError:
+            print(f"Please enter valid integer")
+    continent_choice = continent_options[continent_number - 1] # Converts user choice to continent
+    
+    # Adds all supported cities in chosen continent to list
+    for location in LOCATIONS:
+        if '/' in location and location.startswith(continent_choice):
+            continent, city = location.split('/', 1)
+            if '/' in city:
+                if city.split('/')[0]not in cities:
+                    cities.append(city.split('/')[0])
+            else:
+                if city not in cities:
+                    cities.append(city)
+
+    # Prints all supported cities
+    print(f"Cities in {continent_choice}, select a number corresponding with your choice:")
+    for index, city in enumerate(cities, start=1):
+        print(f"{index}. {city}")
+
+    while True:
+        try:
+            city_number = int(input("City: "))
+            if city_number > 0 and city_number <= len(cities):
+                break
+            else:
+                print(f"Invalid input, please enter a number between 1-{len(cities)}.")
+        except:
+            print(f"Please enter valid integer")
+    city = cities[city_number - 1]
+
+    # asks user for second city if the first city they entered are in below list
+    # pytz module has different settings for these below places
+    if city in ['Argentina', 'North_Dakota', 'Kentucky', 'Indiana']:
+        for location in LOCATIONS:
+            if location.startswith(f"{continent_choice}/{city}") and location not in cities2:
+                cities2.append(location.split('/')[2])
+        for index, city in (enumerate(cities2, start=1)):
+            print(f"{index}. {city}")
+
+        # returns inputs to class methods
+        while True:
+            try:
+                city2_number = int(input("City: "))
+                if city2_number > 0 and city2_number <= len(cities2):
+                    break
+                else:
+                    print(f"Invalid input, please enter a number between 1-{len(cities2)}.")
+            except:
+                print(f"Please enter valid integer")
+        city2 = cities2[city2_number - 1]
+        return city, city2, continent_choice
+    
+    # If city does need extra information
+    city2 = False # assert city2 to exist with no value
+    return city, city2, continent_choice
 
 if __name__ == "__main__":
     main()
